1a956f94ae19a2927f3491f306a32d9be585dae3
Andersson Gonçalves <anderssonsilva793@gmail.com>
2022-03-01 10:43:27 -0300

    Melhorias no Header

--- src/Componetes/Header/index.js
+++ src/Componetes/Header/index.js
@@ -10,17 +10,18 @@ export default function Header(){
          <Col offset='m2' s={12} m={8} className='Header grey lighten-5 z-depth-2 center-align'>
             <Row>
                <Col offset='m2' s={12} m={8}>
-                  <h1>Pesquise aqui o seu CEP</h1>
-                  <p>Coloque aqui um endereço para pesquisa.</p>
-                  <form>
-                     <TextInput m={2}/>
-                     <TextInput m={5}/>
-                     <TextInput m={5}/>
-                     <Button waves='light'>
-                     Pesquisar
-                        <Icon right>search</Icon>
-                     </Button>
-                  </form>
+                  <header>
+                     <h1 className='teal-text'>Pesquise aqui o seu CEP</h1>
+                     <p>Coloque aqui um endereço para pesquisa.</p>
+                  </header>
+                  {/* inputs */}
+                  <TextInput label='UF' placeholder='ex. SP' s={12} m={2}/>
+                  <TextInput label='Cidade' s={12} m={5}/>
+                  <TextInput label='Rua' s={12} m={5}/>
+                  <Button waves='light'>
+                  Pesquisar
+                     <Icon right>search</Icon>
+                  </Button>
                </Col>
             </Row>
          </Col>
