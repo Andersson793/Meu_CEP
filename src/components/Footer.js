a39e707a2d283cf0c23efa2c2709c342208149b7
Andersson Silva Gonçalves <anderssonsilva793@gmail.com>
2023-07-09 16:05:52 -0300

    adicionando vite e atualizando dependencias

--- src/components/Footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return(
-        <div className="text-center mt-10">
-            <i>Resultados gerados por <a href="">ViaCep API</a></i>
-        </div>
-    )
-}
